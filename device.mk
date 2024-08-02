7d65c10d3b868e0fe6db6c2a12f4b8bdc2fa9340
Rafiester <rafi.haqqi@gmail.com>
2024-08-02 17:34:23 +0200

    stone: dont build qcom power hal
    Add TARGET_PROVIDES_POWERHAL := true
    to avoid conflict with caf powerhal

--- device.mk
+++ device.mk
@@ -381,6 +381,19 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
+
+TARGET_PROVIDES_POWERHAL := true
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
