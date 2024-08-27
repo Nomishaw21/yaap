74081f65b8544c1769b26791bc9b9f3158fa0ac4
kamikaonashi <thorschi93@gmx.at>
2024-08-27 18:42:43 +0200

    stone: add path for prebuilt kernel headers

--- device.mk
+++ device.mk
@@ -278,6 +278,7 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_ENABLE_UFFD_GC := false
+PRODUCT_VENDOR_KERNEL_HEADERS := device/xiaomi/stone-kernel/kernel-headers
 
 # Keymaster
 PRODUCT_PACKAGES += \
