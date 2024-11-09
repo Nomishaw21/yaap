93484bd1871a90706052e03bee638e8d6209b378
Nomishaw21 <nomishaw9@gmail.com>
2024-11-09 14:18:24 +0530

    stone: Bring up Yaap

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_stone.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_stone-user \
-    lineage_stone-userdebug \
-    lineage_stone-eng
+    $(LOCAL_DIR)/yaap_stone.mk
